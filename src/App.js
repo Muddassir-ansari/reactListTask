fddf12c3bfbfe1b53aad6a0d3778041213aad5de
Muddassir-ansari <muddassiransari06@gmail.com>
2022-10-18 18:18:45 +0100

    Final Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Ques1 from './Task1/Ques1';
+import Ques2 from './Task2/Ques2';
+import Ques3 from './Task3/Ques3';
+import Ques4 from './Task4/Ques4';
+
+function App() {
+  return (
+    <div>
+      <Ques1/>
+      <Ques2/>
+      <Ques3/>
+      <Ques4/>
+    </div>
+  );
+}
+
+export default App;
